080d0e00d63249a6e5165a4cc4119c2d732b64ee
igor-brishkoski <igor.brishkoski@gmail.com>
2016-08-08 15:50:59 -0400

    Inital commit

---
+++ app/src/test/java/dragon/com/jenkinspipe/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package dragon.com.jenkinspipe;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
